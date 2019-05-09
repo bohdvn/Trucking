13abf397dd25cb1c6ad1ac232f7fc0c4a2140905
Dmitry-Gorlach <harlachdzmitry@gmail.com>
2019-05-09 19:31:30 +0300

    creating REST methods for ClientCompanyController, InvoiceController, ProductController, WayBillController

--- Server/src/main/java/by/itechart/Server/service/InvoiceService.java
+++ Server/src/main/java/by/itechart/Server/service/InvoiceService.java
@@ -1,4 +1,17 @@
 package by.itechart.Server.service;
 
+import by.itechart.Server.entity.Invoice;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface InvoiceService {
+
+    void save(Invoice invoice);
+
+    List<Invoice> findAll();
+
+    Optional<Invoice> findById(int id);
+
+    void deleteById(int id);
 }
--- Server/src/main/java/by/itechart/Server/service/WayBillService.java
+++ Server/src/main/java/by/itechart/Server/service/WayBillService.java
@@ -1,4 +1,17 @@
 package by.itechart.Server.service;
 
+import by.itechart.Server.entity.WayBill;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface WayBillService {
+
+    void save(WayBill wayBill);
+
+    List<WayBill> findAll();
+
+    Optional<WayBill> findById(int id);
+
+    void deleteById(int id);
 }

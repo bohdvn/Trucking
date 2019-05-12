0ee939d1623e101e27d0539d127424a533e1670b
Dmitry-Gorlach <harlachdzmitry@gmail.com>
2019-05-12 09:05:39 +0300

    fix service test

--- Server/src/main/java/by/itechart/Server/controller/AddressController.java
+++ Server/src/main/java/by/itechart/Server/controller/AddressController.java
@@ -6,15 +6,10 @@ import by.itechart.Server.service.AddressService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/address")

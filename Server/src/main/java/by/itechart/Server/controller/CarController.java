0ec6d6b7dbe46791132184c938b0c8b1e7372e65
Victor Bogdan <gunnerinho@gmail.com>
2019-05-12 19:38:23 +0300

    slf4j

--- Server/src/main/java/by/itechart/Server/controller/CarController.java
+++ Server/src/main/java/by/itechart/Server/controller/CarController.java
@@ -4,6 +4,8 @@ import by.itechart.Server.dto.CarDto;
 import by.itechart.Server.entity.Car;
 import by.itechart.Server.entity.User;
 import by.itechart.Server.service.CarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +23,18 @@ public class CarController {
         this.carService=carService;
     }
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CarController.class);
+
     @PutMapping("/")
     public ResponseEntity<?> create(@RequestBody Car car){
+        LOGGER.info("REST request. Path:/car method: POST. car: {}", car);
         carService.save(car);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getOne(@PathVariable("id") int id){
+        LOGGER.info("REST request. Path:/car/{} method: GET.", id);
         Optional<Car> car = carService.findById(id);
         return car.isPresent()?
                 ResponseEntity.ok().body(car.get().transform()):
@@ -37,14 +43,17 @@ public class CarController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> remove(@PathVariable("id") int id){
-            carService.deleteById(id);
+        LOGGER.info("REST request. Path:/car/{} method: DELETE.", id);
+        carService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<CarDto>> getAll() {
+        LOGGER.info("REST request. Path:/car method: GET.");
         List<Car> cars = carService.findAll();
         List<CarDto> carsDto = cars.stream().map(Car::transform).collect(Collectors.toList());
+        LOGGER.info("Return carList.size:{}", carsDto.size());
         return cars.isEmpty() ? new ResponseEntity<>(HttpStatus.NO_CONTENT) :
                new ResponseEntity<>(carsDto,
                        HttpStatus.OK);

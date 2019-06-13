b7ee2113eb78d06909808726ef9633d3ce771869
Dmitry-Gorlach <harlachdzmitry@gmail.com>
2019-06-13 10:49:44 +0300

    update SendEmail component, added validation

--- client/src/components/forms/SendEmail.js
+++ client/src/components/forms/SendEmail.js
@@ -21,26 +21,66 @@ class SendEmail extends Component {
         super(props);
         this.state = {
             email: this.emptyEmail,
-            resultMessage: ''
+            resultMessage: '',
+            subject: '',
+            message: '',
+            formErrors: {subject: '', message: ''},
+            subjectValid: false,
+            messageValid: false,
+            formValid: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
+    handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
         let email = {...this.state.email};
         email[name] = value;
-        this.setState({email});
-        // this.setState({value: event.target.value});
-        // console.log(event.target.value)
-        console.log(this.state);
+        this.setState({email}, () => {
+            this.validateField(name, value);
+            console.log("New state in ASYNC callback:", this.state.email)
+        });
+    };
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let subjectValid = this.state.subjectValid;
+        let messageValid = this.state.messageValid;
+
+        switch (fieldName) {
+            case 'subject':
+                subjectValid = value.length >= 2 && value.length <= 150;
+                fieldValidationErrors.subject = subjectValid ? '' : ' - тема не заполнена (от 1 до 150 символов)';
+                break;
+            case 'message':
+                messageValid = value.length >= 5 && value.length <= 500;
+                fieldValidationErrors.message = messageValid ? '' : ' - слишком короткое сообщение (от 5 до 500 символов)';
+                break;
+            default:
+                break;
+        }
+        this.setState({
+            formErrors: fieldValidationErrors,
+            subjectValid: subjectValid,
+            messageValid: messageValid
+        }, this.validateForm);
+    }
+
+    validateForm() {
+        this.setState({
+            formValid:
+                this.state.subjectValid &&
+                this.state.messageValid
+        });
+    }
+
+    errorClass(error) {
+        return (error.length === 0 ? '' : 'has-error');
     }
 
     setRecipientsNames = () => {
-        console.log(this.state.email.selectedUsers);
         const recipients = this.state.email.recipients.map(user => {
             return <li className="Success" key={user.id}>
                 {user.surname} {user.name} {user.patronymic} ({user.email})
@@ -86,21 +126,30 @@ class SendEmail extends Component {
                 <br/>
                 <p> {RECIPIENTS}</p>
                 {this.setRecipientsNames()}
+                <br/>
                 <FormGroup>
-                    <Label for="subject">{SUBJECT}</Label>
-                    <Input
-                        type="text"
-                        name="subject"
-                        onChange={this.handleChange}/>
+                    <div className={`form-group ${this.errorClass(this.state.formErrors.subject)}`}>
+                        <Label for="subject">{SUBJECT}</Label>
+                        <Input
+                            type="text"
+                            name="subject"
+                            onChange={this.handleChange}/>
+                    </div>
+                    <p className={'error-message'}>{(this.state.formErrors.subjectValid === '') ? ''
+                        : this.state.formErrors.subject}</p>
                 </FormGroup>
                 <FormGroup>
-                    <Label for="message">{MESSAGE}</Label>
-                    <Input
-                        type="textarea"
-                        name="message"
-                        onChange={this.handleChange}/>
+                    <div className={`form-group ${this.errorClass(this.state.formErrors.message)}`}>
+                        <Label for="message">{MESSAGE}</Label>
+                        <Input
+                            type="textarea"
+                            name="message"
+                            onChange={this.handleChange}/>
+                    </div>
+                    <p className={'error-message'}>{(this.state.formErrors.messageValid === '') ? ''
+                        : this.state.formErrors.message}</p>
                 </FormGroup>
-                <Button>{SEND}</Button>
+                <Button type="submit" className="btn btn-primary" disabled={!this.state.formValid}>{SEND}</Button>
             </Form>
         )
     }
